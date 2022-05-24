a78c8f4767e890e24ade99785f24718ba30e6a56
Ralitsa Voronevska <ralicavoronevska@gmail.com>
2022-05-24 21:12:26 +0300

    Committing the finalized Vue 2 Todo List v.1.0.0

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
-# vue-2-todo-list
+# my-first-vue-2-project
+
+This is the TodoList app v1.0.0.
+
+It is part of the [Vue JS Crash Course (2019) [Traversy Media]](https://www.youtube.com/watch?v=Wy9q22isx3U).
+And it is deployed using Netlify following [Deploy Websites In Seconds With Netlify [Traversy Media]](https://youtu.be/bjVUqvcCnxM)
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComponent />
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HeaderComponent from "./components/layout/Header.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeaderComponent,
+  },
+};
 </script>
 
 <style>
@@ -23,6 +22,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
